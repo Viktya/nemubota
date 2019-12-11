33b505ce8d228a57cc44f10327c64543b7d95727
Francesca Peres <32465921+Viktya@users.noreply.github.com>
2019-12-11 18:40:08 +0100

    ;

--- commands/kick.js
+++ commands/kick.js
@@ -2,15 +2,15 @@ module.exports = msg => {
     const member = msg.mentions.members.first()
 
     if (!member) {
-        return msg.reply(`Who are you trying to kick? You must mention a user.`)
+        return msg.reply(`Who are you trying to kick? You must mention a user.`);
     }
   
     if (!member.kickable) {
-        return msg.reply(`I can't kick this user. Sorry!`)
+        return msg.reply(`I can't kick this user. Sorry!`);
     }
   
     return member
     .kick()
     .then(() => msg.reply(`${member.user.tag} was kicked.`))
-    .catch(error => msg.reply(`Sorry, an error occured.`))
+    .catch(error => msg.reply(`Sorry, an error occured.`));
 }
